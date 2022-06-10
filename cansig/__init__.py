f267839d28c91d877c9c6dcb6839fca4d6d93d80
Josephine Yates <47554333+josephineyates@users.noreply.github.com>
2022-06-10 15:51:01 +0200

    modif init

--- cansig/__init__.py
+++ cansig/__init__.py
@@ -1,22 +1,13 @@
 import cansig.cluster.leiden
-import cansig.cluster.api  # noqa: F401
-
-import cansig.cnvanalysis.differentialcnvs  # noqa: F401
-
+import cansig.cnvanalysis.differentialcnvs
+import cansig.filesys
+import cansig.gsea
 import cansig.metaanalysis.heatmap
-import cansig.metaanalysis.repr_directory  # noqa: F401
+import cansig.metaanalysis.repr_directory
+import cansig.models.scvi
+import cansig.multirun
+import cansig.plotting.plotting  # noqa F401
 
-import cansig.models.scvi  # noqa: F401
+from cansig._preprocessing.main import preprocessing  # noqa F401
 
-import cansig.plotting.plotting  # noqa: F401
-
-import cansig.run.download
-import cansig.run.format
-import cansig.run.integration
-import cansig.run.pipeline
-import cansig.run.postprocessing
-import cansig.run.heatmap  # noqa: F401
-
-from cansig._preprocessing.main import preprocessing
-
-__all__ = ["preprocessing", "cluster", "cnvanalysis", "metaanalysis", "models", "plotting", "run"]
+# __all__ = ["preprocessing", "cluster", "cnvanalysis", "metaanalysis", "models", "plotting", "run"]
