21cf967b0b7ced88a967b479470447b20058646a
Josephine Yates <47554333+josephineyates@users.noreply.github.com>
2022-08-30 12:06:27 +0200

    init

--- src/cansig/__init__.py
+++ src/cansig/__init__.py
@@ -7,7 +7,17 @@ import cansig.metaanalysis.repr_directory
 import cansig.models.scvi
 import cansig.multirun
 import cansig.plotting.plotting  # noqa F401
+import cansig.metasignatures  # noqa F401
 
 from cansig._preprocessing.main import preprocessing  # noqa F401
 
-__all__ = ["preprocessing", "cluster", "cnvanalysis", "metaanalysis", "models", "plotting", "run"]
+__all__ = [
+    "preprocessing",
+    "cluster",
+    "cnvanalysis",
+    "metaanalysis",
+    "models",
+    "plotting",
+    "run",
+    "metasignatures",
+]
