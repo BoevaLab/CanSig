38e77ec6ccf2fde23168bd2db85a3c1fe5fac7c7
Josephine Yates <47554333+josephineyates@users.noreply.github.com>
2023-01-25 12:23:17 +0100

    docstrings and minor

--- src/cansig/metasignatures/utils.py
+++ src/cansig/metasignatures/utils.py
@@ -17,6 +17,33 @@ import cansig.plotting.plotting as plotting  # pytype: disable=import-error
 
 
 def get_runs_sig(basedir: pl.Path, n_genes: int = 50, q_thresh: float = 0.005) -> Dict[str, List]:
+    """Obtain all the information about the signatures obtained in preprocessing
+
+    Args:
+
+        basedir: path to the directory where all postprocessing runs are stored
+        n_genes: for filtering of signatures that are not "strong" enough, number of differentially
+            expressed genes to consider
+        q_thresh: for filtering of signatures that are not "strong" enough, q-value selected for
+            differential expression. If there are less than n_genes differentially expressed
+            in the signature with q<q_thresh, we discard the annotate the signature as weak
+    Returns:
+
+        a dictionary containing
+
+            - signatures: a list with all the signatures (genes are ranked according to diff expression)
+            - integration_runs: the integration run the signature is associated with
+            - runs: the runs the signature is associated with
+            - sig_index: a list with [iteration, n_clusters] for each signature
+            - cluster_memb: a list with the cluster membership for all cells for all iterations
+            - threshold: a list of size (1,) with the automatically computed threshold
+            - passed: a mask list with True if the signature is 'strong' enough
+            - n_genes: a list of size (1,) with the number of genes used for determining if
+                a signature is strong enough
+            - q_thresh: a list of size (1,) with the q threshold used for determining if
+                a signature is strong enough
+
+    """
     signatures = []
     integration_runs = []
     runs = []
@@ -51,6 +78,8 @@ def get_runs_sig(basedir: pl.Path, n_genes: int = 50, q_thresh: float = 0.005) -
         for run_path in sorted(path.joinpath("signatures").iterdir()):
             n_cluster = run_path.name.split("cl")[1].split(".")[0]
             signature = pd.read_csv(run_path, index_col=0)
+            # look at if the signature is "strong" i.e. has enough genes diff expressed above a
+            # specific q-value threshold
             sig = signature[(signature.qvals < q_thresh) & (signature.zscores > 0)]
             passed.append((True if sig.shape[0] > n_genes else False))
             signature = signature.index.tolist()
@@ -59,6 +88,9 @@ def get_runs_sig(basedir: pl.Path, n_genes: int = 50, q_thresh: float = 0.005) -
             runs.append(n_run)
 
         unique_runs = np.unique(runs)
+        # this is an automatically computed threshold that can be used so that we only filter out runs that
+        # would most likely be an artifact. Can be used at the meta-signature clustering phase for outlier
+        # detection
         threshold = (
             1 / (len(unique_runs)) * (len(n_clusters) * len(cluster_rs)) / (len(cluster_rs) * np.sum(list(n_clusters)))
         )
@@ -117,6 +149,19 @@ def save_cell_metamembership(metamembership: pd.DataFrame, prob_metamembership: 
 
 
 def score_sig(adata: ad.AnnData, signature: Union[np.ndarray, List[str]], score_name: str):
+    """Scores the adata according to the provided signatures
+
+    Args:
+
+        adata: original adata on which the analysis is performed
+        signature: a list of genes that define a signature
+        score_name: the name of the score in the adata
+
+    Returns:
+
+        adata with the signature scored
+
+    """
     adata.layers["counts"] = adata.X.copy()
     sc.pp.normalize_total(adata, target_sum=10000)
     sc.pp.log1p(adata)
@@ -127,11 +172,22 @@ def score_sig(adata: ad.AnnData, signature: Union[np.ndarray, List[str]], score_
     return adata
 
 
-def rename_metasig(meta_signatures):
+def rename_metasig(meta_signatures: Dict[str, np.ndarray]) -> Dict[str, np.ndarray]:
+    """Renames the metasignature in a human-understandable way
+
+    Args:
+
+        meta_signatures: a dict containing the results of the metasignatures found
+
+    Returns:
+
+        updated dictionary with new meta-signature names
+
+    """
     nmeta = {}
     for k, v in meta_signatures.items():
         if k >= 0:
-            nmeta[f"metasig{int(k+1)}"] = np.array(v)
+            nmeta[f"metasig{int(k)+1}"] = np.array(v)
         else:
             nmeta["outlier"] = np.array(v)
     return nmeta
@@ -143,7 +199,15 @@ def rename_metasig(meta_signatures):
 def plot_clustermap(
     results: np.ndarray, resdir: pl.Path, filename: str = "clustermap-metasignatures-correlation.png"
 ) -> None:
+    """Plotting function; plots the clustermap to visualize the relationship between the meta-signatures
 
+    Args:
+
+        results: an array containing the correlation between the meta-signatures
+        resdir: the directory in which to save the plot
+        filename: the name of the file to save
+
+    """
     g = sns.clustermap(results, cmap="vlag", center=0)
     g.savefig(resdir / filename, bbox_inches="tight")
 
@@ -151,6 +215,18 @@ def plot_clustermap(
 def plot_heatmap(
     sim: np.ndarray, idx: np.ndarray, resdir: pl.Path, filename: str = "heatmap-metasignatures.png"
 ) -> None:
+
+    """Plotting function; plots the heatmap to visualize the pairwise similarities between signatures
+        ordered by meta-signature membership
+
+    Args:
+
+        sim: an array of size (n_signatures,n_signatures) containing the pairwise similarity between signatures
+        idx: the indices of the signatures sorted by cluster assignment
+        resdir: the directory in which to save the plot
+        filename: the name of the file to save
+
+    """
     fig, ax = plt.subplots(figsize=(10, 10))
     sns.heatmap(sim[np.ix_(idx, idx)], ax=ax, cmap="vlag", center=0)
     plt.tick_params(
@@ -173,6 +249,21 @@ def viz_clusters_runs(
     spread: float = 1.5,
     filename: str = "viz-clusters-runs.png",
 ) -> None:
+
+    """Plotting function; plots the signatures in MDS and UMAP space, colored according to the run of origin
+        and the meta-signature it belongs to
+
+    Args:
+
+        sim: an array of size (n_signatures,n_signatures) containing the pairwise similarity between signatures
+        clusters: an array of cluster assignments for each signature
+        runs: the runs the signatures originate from
+        resdir: the directory in which to save the plot
+        min_dist: min_dist parameter for UMAP
+        spread: spread parameter for UMAP
+        filename: the name of the file to save
+
+    """
 
     reduce_umap = umap.UMAP(min_dist=min_dist, spread=spread, metric="precomputed")
     reduce_mds = MDS(n_components=2, dissimilarity="precomputed")
@@ -223,6 +314,24 @@ def plot_metamembership(
     batch_column: str,
 ) -> None:
 
+    """Plotting function; plots the latent spaces colored according (a) to the hard meta-membership labeling
+        and (b) to the soft labeling, i.e., the probability of a cell to belong to the meta-membership
+
+    Args:
+
+        adata: the original adata on which the analysis is run
+        metamembership: pd.Df containing the hard meta-membership assignment of cells
+        prob_metamembership: pd.Df containing the probability of belonging to each meta-signature for
+            each cell
+        integration_path: the path to the directory where the latent representation for the integration run is stored
+        resdir: the directory in which to save the plot
+        batch_column: the name of the column where the batch information is stored
+
+    See also:
+        clustering.get_cell_metamembership, the function that generates the metamembership and prob_metamembership
+            dataframe
+    """
+
     latent_representations = pd.read_csv(integration_path / "latent-representations.csv", index_col=0, header=None)
 
     adata_copy = adata.copy()
@@ -251,7 +360,17 @@ def plot_score_UMAP(
     len_sig: int = 1000,
     filename: str = "score-space-UMAP.png",
 ) -> None:
+    """Plotting function; plots the UMAP representation of the cells in the score space
 
+    Args:
+
+        adata: the original adata on which the analysis is run
+        meta_signatures: dictionary containing the metasignature name as key and the ordered gene list as value
+        resdir: the directory in which to save the plot
+        len_sig: the number of genes to consider for scoring for the signature
+        filename: name of the file to save
+
+    """
     sigs_cansig = {}
     for k, v in meta_signatures.items():
         sigs_cansig[k] = v[: min(len_sig, len(v))]
