47d4a4630707ab0a08a8580ce8199b9c2a12797c
Martin Kim <martinkim@berkeley.edu>
2023-02-02 13:30:19 -0800

    Refresh CanSigTrainingPlan

--- src/cansig/integration/training/training_plan.py
+++ src/cansig/integration/training/training_plan.py
@@ -1,12 +1,10 @@
-from inspect import getfullargspec
-from typing import Optional, Literal  # pytype: disable=not-supported-yet
+from typing import Literal, Optional  # pytype: disable=not-supported-yet
 
-import pytorch_lightning as pl  # pytype: disable=import-error
 import torch  # pytype: disable=import-error
 from scvi.module.base import LossOutput  # pytype: disable=import-error
+from scvi.train import TrainingPlan  # pytype: disable=import-error
 from scvi.train._metrics import ElboMetric  # pytype: disable=import-error
-from torch.optim.lr_scheduler import ReduceLROnPlateau  # pytype: disable=import-error
-
+from scvi.train._trainingplans import TorchOptimizerCreator
 
 from cansig.integration.base.module import CanSigBaseModule  # pytype: disable=import-error
 
@@ -66,16 +64,18 @@ def _cycle_annealing(
 # Ideally, we would inherit from scVI's trainingsplan, however, it calls the kl_weight
 # in the initializer and since CanSigTrainingPlan is not yet initialized it doesn't find
 # the property. There is probably a way of solving it, but I don't know how.
-class CanSigTrainingPlan(pl.LightningModule):
+class CanSigTrainingPlan(TrainingPlan):
     def __init__(
         self,
         module: CanSigBaseModule,
-        optimizer: Literal["Adam", "AdamW"] = "Adam",
-        beta: float = 1.0,
+        *,
+        optimizer: Literal["Adam", "AdamW", "Custom"] = "Adam",
+        optimizer_creator: Optional[TorchOptimizerCreator] = None,
+        beta: float = 1.0,  # new 
         annealing: Literal["linear", "cyclical"] = "linear",
+        lr: float = 1e-3,
+        weight_decay: float = 1e-6,
         eps: float = 0.01,
-        lr=1e-3,
-        weight_decay=1e-6,
         n_steps_kl_warmup: Optional[int] = None,
         n_epochs_kl_warmup: Optional[int] = 400,
         reduce_lr_on_plateau: bool = False,
@@ -88,34 +88,25 @@ class CanSigTrainingPlan(pl.LightningModule):
         lr_min: Optional[float] = None,
         **loss_kwargs,
     ):
-        super().__init__()
+        super().__init__(
+            module,
+            optimizer=optimizer,
+            optimizer_creator=optimizer_creator,
+            lr=lr,
+            weight_decay=weight_decay,
+            eps=eps,
+            n_steps_kl_warmup=n_steps_kl_warmup,
+            n_epochs_kl_warmup=n_epochs_kl_warmup,
+            reduce_lr_on_plateau=reduce_lr_on_plateau,
+            lr_factor=lr_factor,
+            lr_patience=lr_patience,
+            lr_scheduler_metric=lr_scheduler_metric,
+            lr_threshold=lr_threshold,
+            lr_min=lr_min,
+            **loss_kwargs,
+        )
         self.annealing = annealing
         self.beta = beta
-        self.module = module
-        self.lr = lr
-        self.weight_decay = weight_decay
-        self.eps = eps
-        self.optimizer_name = optimizer
-        self.n_steps_kl_warmup = n_steps_kl_warmup
-        self.n_epochs_kl_warmup = n_epochs_kl_warmup
-        self.reduce_lr_on_plateau = reduce_lr_on_plateau
-        self.lr_factor = lr_factor
-        self.lr_patience = lr_patience
-        self.lr_scheduler_metric = lr_scheduler_metric
-        self.lr_threshold = lr_threshold
-        self.lr_min = lr_min
-        self.loss_kwargs = loss_kwargs
-
-        self._n_obs_training = None
-        self._n_obs_validation = None
-
-        # automatic handling of kl weight
-        self._loss_args = getfullargspec(self.module.loss)[0]
-        if "kl_weight" in self._loss_args:
-            self.loss_kwargs.update({"kl_weight": self.kl_weight})
-
-        self.initialize_train_metrics()
-        self.initialize_val_metrics()
 
     def initialize_train_metrics(self):
         """Initialize train related metrics."""
@@ -126,48 +117,6 @@ class CanSigTrainingPlan(pl.LightningModule):
         """Initialize train related metrics."""
         self.elbo_val = ElboMetric(self.n_obs_validation, mode="validation")
         self.elbo_val.reset()
-
-    @property
-    def n_obs_training(self):
-        """
-        Number of observations in the training set.
-
-        This will update the loss kwargs for loss rescaling.
-
-        Notes
-        -----
-        This can get set after initialization
-        """
-        return self._n_obs_training
-
-    @n_obs_training.setter
-    def n_obs_training(self, n_obs: int):
-        if "n_obs" in self._loss_args:
-            self.loss_kwargs.update({"n_obs": n_obs})
-        self._n_obs_training = n_obs
-        self.initialize_train_metrics()
-
-    @property
-    def n_obs_validation(self):
-        """
-        Number of observations in the validation set.
-
-        This will update the loss kwargs for loss rescaling.
-
-        Notes
-        -----
-        This can get set after initialization
-        """
-        return self._n_obs_validation
-
-    @n_obs_validation.setter
-    def n_obs_validation(self, n_obs: int):
-        self._n_obs_validation = n_obs
-        self.initialize_val_metrics()
-
-    def forward(self, *args, **kwargs):
-        """Passthrough to `model.forward()`."""
-        return self.module(*args, **kwargs)
 
     def training_step(self, batch, batch_idx, optimizer_idx=0):
         if "kl_weight" in self.loss_kwargs:
@@ -184,34 +133,6 @@ class CanSigTrainingPlan(pl.LightningModule):
         _, _, scvi_loss = self.forward(batch, loss_kwargs=self.loss_kwargs)
         self.log("validation_loss", scvi_loss.loss, on_epoch=True)
         self.compute_and_log_metrics(scvi_loss, self.elbo_val)
-
-    def configure_optimizers(self):
-        params = filter(lambda p: p.requires_grad, self.module.parameters())
-        if self.optimizer_name == "Adam":
-            optim_cls = torch.optim.Adam  # pytype: disable=module-attr
-        elif self.optimizer_name == "AdamW":
-            optim_cls = torch.optim.AdamW  # pytype: disable=module-attr
-        else:
-            raise ValueError("Optimizer not understood.")
-        optimizer = optim_cls(params, lr=self.lr, eps=self.eps, weight_decay=self.weight_decay)
-        config = {"optimizer": optimizer}
-        if self.reduce_lr_on_plateau:
-            scheduler = ReduceLROnPlateau(
-                optimizer,
-                patience=self.lr_patience,
-                factor=self.lr_factor,
-                threshold=self.lr_threshold,
-                min_lr=self.lr_min,
-                threshold_mode="abs",
-                verbose=True,
-            )
-            config.update(
-                {
-                    "lr_scheduler": scheduler,
-                    "monitor": self.lr_scheduler_metric,
-                },
-            )
-        return config
 
     @property
     def kl_weight(self):
