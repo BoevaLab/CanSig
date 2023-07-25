7219ec9ee4f1c3a657cc273ad82772819da96ba3
FlorianBarkmann <flobarkmann@googlemail.com>
2023-07-25 23:25:34 +0200

    Updated precommit.

--- src/cansig/utils.py
+++ src/cansig/utils.py
@@ -3,7 +3,5 @@ from cansig.types import Pathlike  # pytype: disable=import-error
 
 
 def read_anndata(path: Pathlike) -> sc.AnnData:
-    # Workaround for a bug in scanpy.
     adata = sc.read_h5ad(path)
-    adata.uns["log1p"] = {"base": None}
     return adata
